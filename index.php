79c4dd1d21b4dcc74fa61892652b1688b308b92e
Sakonokode <maxence.attal@gmail.com>
2018-07-17 17:46:44 +0200

    Add of PDO, connect sucessfully to the db. Add of tables in db

--- index.php
+++ index.php
@@ -9,9 +9,11 @@
 ini_set('display_errors', 1);
 
 require_once __DIR__ . '/src/model/register.php';
+require_once __DIR__ . '/src/controller/PostsController.php';
 
 echo("<pre>");
+$postsController = new PostsController();
+$postsController->getPosts();
+
 var_dump($_SERVER["REQUEST_URI"]);
 echo("</pre>");
-
-
--- src/controller/PostsController.php
+++ src/controller/PostsController.php
@@ -6,14 +6,18 @@
  * Time: 19:06
  */
 
-function getPosts() {
+class PostsController {
 
-    try {
-        $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8\', \'root\', \'root\'');
-    }
+    function getPosts() {
 
-    catch(Exception $e) {
-        die('Error : '.$e->getMessage());
+        try {
+            $db = new PDO('mysql:host=127.0.0.1;dbname=blogwritter;charset=utf8', "phpmyadmin", "root");
+            // set the PDO error mode to exception
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
+        }
+        catch(Exception $e) {
+            die('Error : '. $e->getMessage());
+        }
     }
 }
-
