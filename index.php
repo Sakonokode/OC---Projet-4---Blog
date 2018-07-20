80dc6b98560c573dc889fe2745b29cac9b9a1781
Sakonokode <maxence.attal@gmail.com>
2018-07-20 19:34:04 +0200

    Setting composer, installed twig lib, beginning to set routes

--- index.php
+++ index.php
@@ -8,8 +8,13 @@
 
 ini_set('display_errors', 1);
 
+require_once "vendor/autoload.php";
 require_once __DIR__ . '/src/model/register.php';
 require_once __DIR__ . '/src/controller/PostsController.php';
+
+$loader = new Twig_Loader_Array(array('index' => 'Hello {{name}}!',));
+$twig = new Twig_Environment($loader);
+echo $twig->render('index', array('name' => 'Fabien'));
 
 echo("<pre>");
 $postsController = new PostsController();
--- src/controller/routes.php
+++ src/controller/routes.php
@@ -6,19 +6,9 @@
  * Time: 17:37
  */
 
-if (isset($_GET['action'])) {
-    if ($_GET['action'] == 'listPosts') {
-        //On appel le controlleur qui s'occupe de renvoyer la liste des posts
-        echo ("test de condition remplie");
-    }
-    elseif ($_GET['action'] == 'readPost') {
-        if (isset($_GET['id']) && $_GET['id'] > 0) {
-            //On appel le controlleur qui renvoie le post demande par l user
-            echo ("test de condition remplie");
+namespace App;
 
-        }
-    }
-}
+$router = new App;
 
 
 // Ecrire le fichier .htaccess a la racine pour rediriger toutes les requetes http entrantes vers index.php qui pourra recuperer l'url avec les / grace a
